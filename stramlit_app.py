ef906261b4eb98870dc47d59d36096c3a60f1d64
Priyaaa1298 <priyankakharade1298@gmail.com>
2025-02-13 16:04:29 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -60,9 +60,9 @@ if uploaded_file:
     st.pyplot(fig)
     
     # Feature Selection
-    X = df_cleaned[['ambient', 'u_d', 'u_q', 'i_d', 'i_q', 'pm', 'stator_winding']]
-    y = df_cleaned['motor_speed']
-    
+    X = data[['ambient','u_d','u_q', 'i_d', 'i_q', 'pm','stator_winding','torque','coolant']]
+    y = data['motor_speed']
+
     # Train-Test Split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
